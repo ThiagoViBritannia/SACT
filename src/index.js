bde4b080c669b5ec726e280a98c327232c4db0f0
thvzsd <tinhosv@protonmail.com>
2019-12-07 20:37:59 -0400

    merge from repo

--- src/index.js
+++ src/index.js
@@ -4,40 +4,10 @@ import './index.css';
 
 import Lister from './screen/Lister';
 import Login from './screen/Login';
-<<<<<<< HEAD
-import Seção from './screen/Seção';
-import Dash from './screen/dashboard';
-import Avcad from './screen/AddAval';
-import AddAdmin from './screen/AddAdmin'
-
-var type = "administrator";
-
-class WindowManager extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { windows: [<Lister name={type} />, <Avcad />] };   
-        this.handleBackWindow = this.handleBackWindow.bind(this)
-    }
-    renderBack() {
-        return (
-            <button id="back" onClick={this.handleBackWindow}></button>
-        );
-    }
-    handleBackWindow(e) {
-        console.log(e)
-        this.setState({
-            windows: this.state.windows.pop()
-        })
-    }
-=======
 import WindowManager from './screen/WindowManager';
 
 import * as serviceWorker from './serviceWorker';
 import * as DBValues from './db/Values';
->>>>>>> 7272041cb9017ee3e2ce673eb41321f9b9fa9a0a
-
-
-var type = "administrator";
 
 class Main extends WindowManager {
   constructor(props) {
