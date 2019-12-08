c7a3f61282540307a398c2876a7eee15bccc3583
thvzsd <tinhosv@protonmail.com>
2019-12-07 20:35:35 -0400

    simple refects

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
 import Lister from './screen/Lister';
 import Login from './screen/Login';
 import WindowManager from './screen/WindowManager';
