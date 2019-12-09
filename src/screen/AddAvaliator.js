26725c76c0114202128553d89d9cec55df035a8a
thvzsd <tinhosv@protonmail.com>
2019-12-09 10:13:17 -0400

    litle ajusts

--- src/screen/AddAvaliator.js
+++ src/screen/AddAvaliator.js
@@ -1,9 +1,11 @@
 import React from 'react';
+
 import  '../css/AddAvaliator.css';
+
 import mais from '../css/assets/mais.png';
 import lix from '../css/assets/lix.png';
-import Box from './component/Box';
-class AddAvaliator extends React.Component{;
+
+class AddAvaliator extends React.Component{
   constructor() {
     super();
     this.state = {
@@ -39,75 +41,86 @@ class AddAvaliator extends React.Component{;
       shareholders: this.state.shareholders.filter((s, sidx) => idx !== sidx)
     });
   };
+  
   render(){
   
-  return (
-    <div className="App">
-    
-  <div align="center">
-<h1 align="center">
-  Cadastro de Avaliadores
-    
-  <hr style={{width:'100%',marginTop:"-0.6%"}} ></hr>
-  </h1>
-</div>
-     <table align = "center" style={{width:"100%"}}>
-      <tr >
-        <td style={{width:"50%"}}>
-        <h2> 1. Nome</h2>
-        <input type="text" name="Avaliador" className="Field" placeholder = "" /> 
-        <h2> 2. Area de Atuação </h2>
-        <input type="text" name="Avaliador" className="Field" placeholder = "" /> 
-        
-        <h2> 6. CPF</h2>
-        <input type="text" name="Avaliador" className="Field" placeholder = "" /> 
-        
-        <h2> 4. Telefone </h2>
-        <input type="text" name="Avaliador" className="Field" placeholder = "" /> 
-        
-        <h2> 5. E-mail </h2>
-        <input type="text" name="Avaliador" className="Field" placeholder = "" /> 
-        <h2> 3. Instituição que representa  </h2>
-        <h6 >
-           <div id='checkboxes'style={{marginTop:"-20%"}}>
-              <input type='checkbox' name='info'   /> Informática <br />
-              <input type='checkbox' name='meca'   /> Mecatrônica <br />
-              <input type='checkbox' name='eletro' /> Eletronica  <br />
-            </div></h6></td>
-            <td  style={{width:"50%"}}>
-        <h2 style={{position:"static"}}> 7. Projetos que irá avaliar </h2>
-        <form onSubmit={this.handleSubmit} >
-        
-        {this.state.shareholders.map((shareholder, idx) => (
-          <div className="shareholder">
-            <input
-              type="text"
-              value={shareholder.name}
-              name="usuario"
-              className="field"
-              onChange={this.handleShareholderNameChange(idx)}
+    return (
+      <div >
+        <div align="center">
+          <h1 align="center">
+            Cadastro de Avaliadores
+            <hr style={{width:'100%',marginTop:"-0.6%"}} />
+          </h1>
+        </div>
+        <table align = "center" style={{width:"100%"}}>
+          <tr>
+            <td style={{width:"50%"}}>
+              <h2> 1. Nome</h2>
+              <input type="text" name="Avaliador" className="Field" placeholder = "" /> 
+            
+              <h2> 2. Area de Atuação </h2>
+              <input type="text" name="Avaliador" className="Field" placeholder = "" /> 
               
-              ></input>
-            <img  align = "right" src={lix} style={{width:"4.5%", marginTop:"2.8%", marginLeft:"-15%"}} onClick={this.handleRemoveShareholder(idx)}></img>
-             </div>
-        ))}
-        <div  align = "center">
-       <img align = "center"src={mais} style={{width:"7%"}}
-          type="button"
-          onClick={this.handleAddShareholder}
-          className="small"
-     
-        /></div>
-      </form>
-      <h5> 8. Status do avaliador </h5>
-        <input type="text" name="Avaliador" className="Field" placeholder = "" /> 
+              <h2> 6. CPF</h2>
+              <input type="text" name="Avaliador" className="Field" placeholder = "" /> 
+              
+              <h2> 4. Telefone </h2>
+              <input type="text" name="Avaliador" className="Field" placeholder = "" /> 
+              
+              <h2> 5. E-mail </h2>
+              <input type="text" name="Avaliador" className="Field" placeholder = "" /> 
+            
+              <h2> 3. Instituição que representa  </h2>
+              <h6>
+                <div id='checkboxes'style={{marginTop:"-20%"}}>
+                    <input type='checkbox' name='info'   /> Informática <br />
+                    <input type='checkbox' name='meca'   /> Mecatrônica <br />
+                    <input type='checkbox' name='eletro' /> Eletronica  <br />
+                  </div>
+              </h6>
+            </td>
+            <td style={{width:"50%"}}>
 
-      <div className="button" align="right" >
-<input type="button"name="login" id="login" value="Salvar" style={{width:"20%"}}></input></div>
-    </td>
-        </tr>
-      </table>
-      
-</div>
-    );}}
-    export default AddAvaliator;
+            <h2 style={{position:"static"}}> 7. Projetos que irá avaliar </h2>
+
+            <form onSubmit={this.handleSubmit} >
+              {this.state.shareholders.map(
+              
+              (shareholder, idx) => (
+                  <div className="shareholder">
+                    <input
+                      type="text"
+                      value={shareholder.name}
+                      name="usuario"
+                      className="field"
+                      onChange={this.handleShareholderNameChange(idx)}
+                    />
+                    <img align="right" src={lix} style={{width:"4.5%", marginTop:"2.8%", marginLeft:"-15%"}} onClick={this.handleRemoveShareholder(idx)}/>
+                  </div>
+                )
+              
+              )}
+                <div align="center">
+                  <img
+                    align = "center"src={mais} style={{width:"7%"}}
+                    type="button"
+                    onClick={this.handleAddShareholder}
+                    className="small"
+                  />
+                </div>
+              </form>
+
+              <h5> 8. Status do avaliador </h5>
+                <input type="text" name="Avaliador" className="Field" placeholder = "" /> 
+
+                <div className="button" align="right" >
+                <input type="button"name="login" id="login" value="Salvar" style={{width:"20%"}}></input></div>
+            </td>
+          </tr>
+        </table>
+      </div>
+    );
+  }
+
+}
+export default AddAvaliator;
--- src/screen/AddProject.js
+++ src/screen/AddProject.js
@@ -1,55 +1,56 @@
 import React from 'react';
-import  '../css/AddProject.css';
+import '../css/AddProject.css';
 import Box from '../screen/component/Box';
 function App() {
   return (
     <div className="App">
-    
-<h1 align="center">
-  Cadastro de projetos
-  <hr style={{width:'50%',marginTop:"-0.6%"}} ></hr></h1>
-<table align = "center" style={{width:"100%"}}>
-      <tr >
-        <td style={{}}>
-        <h2> 1. Nome</h2>
-        <Box horizontal = "15px" vertical="10px"> 
-          <input type="text" name="projeto" className="Field" placeholder = "" /> 
-        </Box>
-        <h2> 2. Area de Atuação </h2>
-        <h6 >
-           <div id='checkboxes'>
-              <h6></h6><input type='checkbox' name='info'   /> Informática <br />
-              <h6></h6><input type='checkbox' name='meca'   /> Mecatrônica <br />
-              <h6></h6><input type='checkbox' name='eletro' /> Eletronica  <br />
-            </div></h6>
-        <h2> 3. Turma </h2> 
-        <input type="text" name="projeto" className="Field" placeholder = "" /> 
 
-        </td><td >
-        <h5> 4. Integrantes </h5>
-       <h7>   
-        <input type="text" name="projeto" className="Field" placeholder = "" /> <br/>
-</h7>
-       <h7 style={{marginTop:"100%"}}>  
-        <input type="text" name="projeto" className="Field" placeholder = "" style={{marginTop:"5%"}}/> <br/>
-   </h7>
-       <h7>     
-         
-        <input type="text" name="projeto" className="Field" placeholder = "" style={{marginTop:"5%"}}/> <br/>
- </h7>
-       <h5> 9. Observações </h5> 
-        <input type="text" name="projeto" className="Field" placeholder = "" /> 
-</td>
+      <h1 align="center">
+        Cadastro de projetos
+  <hr style={{ width: '50%', marginTop: "-0.6%" }} ></hr></h1>
+      <table align="center" style={{ width: "100%" }}>
+        <tr >
+          <td style={{}}>
+            <h2> 1. Nome</h2>
+            <Box horizontal="15px" vertical="10px">
+              <input type="text" name="projeto" className="Field" placeholder="" />
+            </Box>
+            <h2> 2. Area de Atuação </h2>
+            <h6 >
+              <div id='checkboxes'>
+                <h6></h6><input type='checkbox' name='info' /> Informática <br />
+                <h6></h6><input type='checkbox' name='meca' /> Mecatrônica <br />
+                <h6></h6><input type='checkbox' name='eletro' /> Eletronica  <br />
+              </div></h6>
+            <h2> 3. Turma </h2>
+            <input type="text" name="projeto" className="Field" placeholder="" />
+
+          </td><td >
+            <h5> 4. Integrantes </h5>
+            <h7>
+              <input type="text" name="projeto" className="Field" placeholder="" /> <br />
+            </h7>
+            <h7 style={{ marginTop: "100%" }}>
+              <input type="text" name="projeto" className="Field" placeholder="" style={{ marginTop: "5%" }} /> <br />
+            </h7>
+            <h7>
+
+              <input type="text" name="projeto" className="Field" placeholder="" style={{ marginTop: "5%" }} /> <br />
+            </h7>
+            <h5> 9. Observações </h5>
+            <input type="text" name="projeto" className="Field" placeholder="" />
+          </td>
         </tr><tr>
-        <td> </td><td>
-          
-      <div className="button" align="right">
-<input type="button"name="login" id="login" value="Salvar"></input></div>
-    </td>
+          <td> </td><td>
+
+            <div className="button" align="right">
+              <input type="button" name="login" id="login" value="Salvar"></input></div>
+          </td>
         </tr>
       </table>
-      
+
     </div>
 
-    );}
-    export default App;
+  );
+}
+export default App;
